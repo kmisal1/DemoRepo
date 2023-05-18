5be959d79d2e1f51641d035d6d17b699bc2f8d25
kmisal1 <kmisal95@gmail.com>
2023-05-18 07:48:30 +0530

    this is git commit

---
+++ src/datatype/DataType.java
@@ -0,0 +1,15 @@
+package datatype;
+
+public class DataType {
+
+	public static void main(String[] args) {
+		
+		int a=12;
+		float b=13;
+		
+		float add=a+b;
+		System.out.println(add);
+
+	}
+
+}
